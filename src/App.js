6b7b73b139b22eaa025bebb00774e65dad6f7f41
ohzuma <ozumanology@gmail.com>
2024-02-27 14:54:38 -0800

    footer

--- src/App.js
+++ src/App.js
@@ -16,6 +16,20 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/detail/:id" element={<Detailed />} />
+        <Route
+          path="*"
+          element={
+            <div className="mt-[15rem] flex flex-col justify-center items-center">
+              <p className="text-2xl font-bold">Page Not Found</p>
+              <p className="text-md">
+                Go back to{" "}
+                <a href={"/"} className="text-xl text-blue-700 font-bold">
+                  home page
+                </a>
+              </p>
+            </div>
+          }
+        />
       </Routes>
       <Footer />
       {/* </LoadingContext> */}
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,36 +3,38 @@ import { Link } from "react-router-dom";
 import { FaFacebook, FaTwitter, FaInstagram, FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className="mt-[5rem] absolute bg-slate-200 w-full py-8 px-5 flex flex-col gap-4 justify-center items-center">
-      {" "}
-      <ul className="flex flex-row gap-3 text-black/80 font-bold  " l>
-        <li className="text-[1rem] ">
-          <a href={"/"}>Home</a>
-        </li>
-        <li className="text-[1rem] ">
-          <a href={"/"}>Cart</a>
-        </li>
-        <li className="text-[1rem] ">
-          <a href={"/"}>Shop</a>
-        </li>
-      </ul>
-      <div className="flex  gap-3 text-[1rem]">
-        <span className="text-[1.4rem] text-black/80">
-          <FaFacebook />
-        </span>
-        <span className="text-[1.4rem] text-black/80">
-          {" "}
-          <FaTwitter />
-        </span>
-        <span className="text-[1.4rem] text-black/80">
-          <FaInstagram />
-        </span>
-        <span className="text-[1.4rem] text-black/80">
-          {" "}
-          <FaGithub />
-        </span>
+    <div className="relative mt-[20rem]">
+      <div className=" absolute bottom-0 bg-slate-200 w-full py-8 px-5 flex flex-col gap-4 justify-center items-center">
+        {" "}
+        <ul className="flex flex-row gap-3 text-black/80 font-bold  " l>
+          <li className="text-[1rem] ">
+            <a href={"/"}>Home</a>
+          </li>
+          <li className="text-[1rem] ">
+            <a href={"/"}>Cart</a>
+          </li>
+          <li className="text-[1rem] ">
+            <a href={"/"}>Shop</a>
+          </li>
+        </ul>
+        <div className="flex  gap-3 text-[1rem]">
+          <span className="text-[1.4rem] text-black/80">
+            <FaFacebook />
+          </span>
+          <span className="text-[1.4rem] text-black/80">
+            {" "}
+            <FaTwitter />
+          </span>
+          <span className="text-[1.4rem] text-black/80">
+            <FaInstagram />
+          </span>
+          <span className="text-[1.4rem] text-black/80">
+            {" "}
+            <FaGithub />
+          </span>
+        </div>
+        <span className="font-bold text-black/80">Thankings For Shoping🎉</span>
       </div>
-      <span className="font-bold text-black/80">Thankings For Shoping🎉</span>
     </div>
   );
 };
